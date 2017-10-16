bd728a9e7d80c0de1544f002c115f6d769efd4e5
Unknown <bhilprecz.com@googlemail.com>
2017-10-16 22:25:18 +0200

    Cors enable

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ module.exports.io = io;
 // middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //DB setup
 var uristring =
