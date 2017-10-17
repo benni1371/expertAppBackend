25db7aa12c21726a395c5cc2ce97597bbd3af6ce
Unknown <bhilprecz.com@googlemail.com>
2017-10-17 23:34:33 +0200

    cleanup

--- config/database.js
+++ config/database.js
@@ -1,6 +1,3 @@
 module.exports = {
-  'secret':'%wCr5f1.q&H`]BZgBTEB',
-  'cloud_name': 'drulajddy', 
-  'api_key': '494751296866136', 
-  'api_secret': 'ge1GVeuBpF5Y2eENDl2BRlBiqww'
+  'secret':'%wCr5f1.q&H`]BZgBTEB'
 };
